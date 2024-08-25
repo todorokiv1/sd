bef737d832410477ba0a61c7a5a0a2836bfbf011
todorokiv1 <155774992+todorokiv1@users.noreply.github.com>
2024-08-25 23:06:45 +0100

    Update run.js

--- run.js
+++ run.js
@@ -1,59 +1,38 @@
-require('dotenv').config();
-const { Client } = require('discord.js-selfbot-v13');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const express = require("express");
+const { exec } = require('child_process');
 
-const channels = ['1202682192698806323'];
+const app = express();
+const PORT = process.env.PORT || 2000;
 
-const checkAndJoin = async (client, channelId) => {
-  try {
-    const channel = client.channels.cache.get(channelId);
-    if (!channel) {
-      console.error('Channel not found with ID: ${channelId}');
-      return;
-    }
+const server = app.listen(PORT, () => {
+  console.log('Your app is listening on port ' + PORT);
+  console.log("I'm Ready To Work..! 24H");
+});
 
-    const guild = channel.guild;
-    const member = guild.members.cache.get(client.user.id);
+app.get('/', (req, res) => {
+  res.send(`
+    <body>
+      <center><h1>Bot 24H ON!</h1></center>
+    </body>
+  `);
+});
 
-    if (!member) {
-      console.error('Bot is not a member of the guild (${guild.name}) associated with the channel ID: ${channelId}');
-      return;
-    }
-
-    const userVoiceState = guild.voiceStates.cache.get(member.id);
-
-    if (!userVoiceState || !userVoiceState.channelId) {
-      const voiceConnection = joinVoiceChannel({
-        channelId: channel.id,
-        guildId: guild.id,
-        adapterCreator: guild.voiceAdapterCreator,
-        selfMute: true,
-        selfDeaf: false,
-      });
-
-      voiceConnection.once('stateChange', (state) => {
-        if (state.status === 'CONNECTED') {
-          console.log('${member.displayName} is in ${guild.name}');
-          console.log('Bot joined voice channel ${channel.name} in ${guild.name}');
-        }
-      });
-
-      voiceConnection.on('error', (error) => {
-        console.error('Error in voice connection:', error);
-      });
-    }
-  } catch (error) {
-    console.error('Error checking and joining:', error);
+exec('concurrently "node server.js" "node answer.js"', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
   }
-};
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
 
-channels.forEach((channelId) => {
-  const client = new Client({ checkUpdate: false });
-
-  client.once('ready', () => {
-    checkAndJoin(client, channelId);
-    setInterval(() => checkAndJoin(client, channelId), 5000);
+process.on('SIGINT', () => {
+  console.log('Server shutting down...');
+  server.close(() => {
+    console.log('Server shut down successfully.');
+    process.exit(0);
   });
-
-  client.login(process.env.token2);
 });
